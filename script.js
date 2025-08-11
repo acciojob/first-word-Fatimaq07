03164f271d239e25b08c35fb4d360ae9f98d3069
FATIMA QURESHI <115686917+Fatimaq07@users.noreply.github.com>
2025-08-11 16:54:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  str = str.trim();
+  const spaceIndex = str.indexOf(' ');
+  if (spaceIndex === -1) {
+    return str;
+  }
+  return str.slice(0, spaceIndex);
 }
+
 
 // Do not change the code below
 
